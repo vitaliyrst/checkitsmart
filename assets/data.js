a83a27ef172710cb2ac183e77b83c7bf9cff6f7d
Vitaliy Klubkou <vitaliy.klubkou@gmail.com>
2021-05-21 15:19:53 +0300

    Add local storage

---
+++ assets/data.js
@@ -0,0 +1,44 @@
+data = {
+    "categories": [
+        {
+            "id": 1,
+            "title": "Комоды и тумбы",
+            "image": "assets/images/category/1.png",
+            "linkModel": "#",
+            "linkSize": "./assets/images/models/table/table_min.usdz",
+            "figure": "assets/images/figures/size.svg"
+        },
+        {
+            "id": 2,
+            "title": "Столы",
+            "image": "assets/images/category/2.png",
+            "linkModel": "./assets/images/models/table/table.usdz",
+            "linkSize": "./assets/images/models/table/table_min.usdz",
+            "figure": "assets/images/figures/size.svg"
+        },
+        {
+            "id": 3,
+            "title": "Кровати",
+            "image": "assets/images/category/3.png",
+            "linkModel": "#",
+            "linkSize": "./assets/images/models/table/table_min.usdz",
+            "figure": "assets/images/figures/size.svg"
+        },
+        {
+            "id": 4,
+            "title": "Шкафы",
+            "image": "assets/images/category/4.png",
+            "linkModel": "#",
+            "linkSize": "./assets/images/models/table/table_min.usdz",
+            "figure": "assets/images/figures/size.svg"
+        },
+        {
+            "id": 5,
+            "title": "Ковры",
+            "image": "assets/images/category/5.png",
+            "linkModel": "./assets/images/models/carpet/carpet.usdz",
+            "linkSize": "./assets/images/models/table/table_min.usdz",
+            "figure": "assets/images/figures/size.svg"
+        },
+    ]
+};
