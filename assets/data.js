859cd54475dc01cdebf08628e2bade0ead856c55
Vitaliy Klubkou <vitaliy.klubkou@gmail.com>
2021-05-24 11:01:08 +0300

    Edit AR Add to cart

--- assets/data.js
+++ assets/data.js
@@ -4,41 +4,46 @@ data = {
             "id": 1,
             "title": "Комоды и тумбы",
             "image": "assets/images/category/1.png",
-            "linkModel": "./assets/images/models/tumba/tumba2.usdz#applePayButtonType=plain&checkoutTitle=Retro%20Alarm%20Clock&checkoutSubtitle=With%20built-in%20FM%20tuner&price=$92.50",
+            "linkModel": "./assets/images/models/tumba/tumba2.usdz#callToAction=Добавить%20в%20корзину&checkoutTitle=Комоды%20и%20тумбы",
             "linkSize": "./assets/images/models/table/table_min.usdz",
-            "figure": "assets/images/figures/size.svg"
+            "figure": "assets/images/figures/size.svg",
+            "linkBuy": "https://catalog.onliner.by/dresser/sokol/t2rsb"
         },
         {
             "id": 2,
             "title": "Столы",
             "image": "assets/images/category/2.png",
-            "linkModel": "./assets/images/models/table/table.usdz#applePayButtonType=plain&checkoutTitle=Retro%20Alarm%20Clock&checkoutSubtitle=With%20built-in%20FM%20tuner&price=$92.50",
+            "linkModel": "./assets/images/models/table/table.usdz#callToAction=Добавить%20в%20корзину&checkoutTitle=Столы",
             "linkSize": "./assets/images/models/table/table_min.usdz",
-            "figure": "assets/images/figures/size.svg"
+            "figure": "assets/images/figures/size.svg",
+            "linkBuy": "https://shop.anrex.by/product/stol_razdvizhnoy_jagger/"
         },
         {
             "id": 3,
             "title": "Кровати",
             "image": "assets/images/category/3.png",
-            "linkModel": "/assets/images/models/bed/Anrex_LINATE.usdz#applePayButtonType=plain&checkoutTitle=Retro%20Alarm%20Clock&checkoutSubtitle=With%20built-in%20FM%20tuner&price=$92.50",
+            "linkModel": "/assets/images/models/bed/Anrex_LINATE.usdz#callToAction=Добавить%20в%20корзину&checkoutTitle=Кровати",
             "linkSize": "./assets/images/models/table/table_min.usdz",
-            "figure": "assets/images/figures/size.svg"
+            "figure": "assets/images/figures/size.svg",
+            "linkBuy": "https://catalog.onliner.by/bed/ikea/70395244"
         },
         {
             "id": 4,
             "title": "Шкафы",
             "image": "assets/images/category/4.png",
-            "linkModel": "./assets/images/models/dresser/dresser.usdz#applePayButtonType=plain&checkoutTitle=Retro%20Alarm%20Clock&checkoutSubtitle=With%20built-in%20FM%20tuner&price=$92.50",
+            "linkModel": "./assets/images/models/dresser/dresser.usdz#callToAction=Добавить%20в%20корзину&checkoutTitle=Шкафы",
             "linkSize": "./assets/images/models/table/table_min.usdz",
-            "figure": "assets/images/figures/size.svg"
+            "figure": "assets/images/figures/size.svg",
+            "linkBuy": "https://catalog.onliner.by/wine_cabinets/liebherr/ewtgb3583"
         },
         {
             "id": 5,
             "title": "Ковры",
             "image": "assets/images/category/5.png",
-            "linkModel": "./assets/images/models/carpet/krug1x1.usdz#applePayButtonType=plain&checkoutTitle=Retro%20Alarm%20Clock&checkoutSubtitle=With%20built-in%20FM%20tuner&price=$92.50",
+            "linkModel": "./assets/images/models/carpet/krug1x1.usdz#callToAction=Добавить%20в%20корзину&checkoutTitle=Ковры",
             "linkSize": "./assets/images/models/table/table_min.usdz",
-            "figure": "assets/images/figures/size.svg"
+            "figure": "assets/images/figures/size.svg",
+            "linkBuy": "https://jysk.by/kover-irbis-522-166-1079-by.html?gclid=Cj0KCQjwna2FBhDPARIsACAEc_Vp2FExssLntFe--DIukpUAula3DCxod-Qmu7VAQXQxj4a1RozAv24aAlWEEALw_wcB"
         },
     ]
 };
