98bfd4fe6125ac19aee6a0c0064b8afdfeac0e7c
Vitaliy Klubkou <vitaliy.klubkou@gmail.com>
2021-05-25 16:37:39 +0300

    Update banner size

--- assets/data.js
+++ assets/data.js
@@ -5,7 +5,7 @@ data = {
             "title": "Комоды и тумбы",
             "image": "assets/images/category/bedside.png",
             "linkModel": "./assets/images/models/bedside/bedside.usdz#custom=https://checkitsmart.com/assets/banner/custom_banner_bedside.html&customHeight=large",
-            "linkSize": "./assets/images/models/bedside/bedside_min.usdz#custom=https://checkitsmart.com/assets/banner/custom_banner_size_bedside.html&customHeight=medium",
+            "linkSize": "./assets/images/models/bedside/bedside_min.usdz#custom=https://checkitsmart.com/assets/banner/size/custom_banner_size_bedside.html&customHeight=medium",
             "figure": "./assets/images/models/bedside/bedside_size.svg",
             "linkBuy": "https://shop.anrex.by/product/tiffany_tumba_3d1s_orekh_eliya_temnyy/",
             "sizes": {w: 139, h: 91, d: 37}
@@ -15,7 +15,7 @@ data = {
             "title": "Столы",
             "image": "assets/images/category/table.png",
             "linkModel": "./assets/images/models/table/table.usdz#custom=https://checkitsmart.com/assets/banner/custom_banner_table.html&customHeight=large",
-            "linkSize": "./assets/images/models/table/table_min.usdz#custom=https://checkitsmart.com/assets/banner/custom_banner_size_table.html&customHeight=medium",
+            "linkSize": "./assets/images/models/table/table_min.usdz#custom=https://checkitsmart.com/assets/banner/size/custom_banner_size_table.html&customHeight=medium",
             "figure": "./assets/images/models/table/table_size.svg",
             "linkBuy": "https://shop.anrex.by/product/stol_razdvizhnoy_jagger/",
             "sizes": {w: 77, h: 80, d: 132}
@@ -25,7 +25,7 @@ data = {
             "title": "Кровати",
             "image": "assets/images/category/bed.png",
             "linkModel": "/assets/images/models/bed/bed.usdz#custom=https://checkitsmart.com/assets/banner/custom_banner_bed.html&customHeight=large",
-            "linkSize": "./assets/images/models/bed/bed_min.usdz#custom=https://checkitsmart.com/assets/banner/custom_banner_size_bed.html&customHeight=medium",
+            "linkSize": "./assets/images/models/bed/bed_min.usdz#custom=https://checkitsmart.com/assets/banner/size/custom_banner_size_bed.html&customHeight=medium",
             "figure": "./assets/images/models/bed/bed_size.svg",
             "linkBuy": "https://shop.anrex.by/product/krovat_linate_160_s_podemnikom_typ_94_01_/",
             "sizes": {w: 164, h: 65, d: 205}
@@ -35,7 +35,7 @@ data = {
             "title": "Шкафы",
             "image": "assets/images/category/dresser.png",
             "linkModel": "./assets/images/models/dresser/dresser.usdz#custom=https://checkitsmart.com/assets/banner/custom_banner_dresser.html&customHeight=large",
-            "linkSize": "./assets/images/models/dresser/dresser_min.usdz#custom=https://checkitsmart.com/assets/banner/custom_banner_size_dresser.html&customHeight=medium",
+            "linkSize": "./assets/images/models/dresser/dresser_min.usdz#custom=https://checkitsmart.com/assets/banner/size/custom_banner_size_dresser.html&customHeight=medium",
             "figure": "./assets/images/models/dresser/dresser_size.svg",
             "linkBuy": "https://shop.anrex.by/product/tiffany_shkaf_1d2s_orekh_eliya_temnyy/",
             "sizes": {w: 69, h: 212, d: 39}
@@ -45,7 +45,7 @@ data = {
             "title": "Ковры",
             "image": "assets/images/category/carpet.png",
             "linkModel": "./assets/images/models/carpet/carpet.usdz#custom=https://checkitsmart.com/assets/banner/custom_banner_carpet.html&customHeight=large",
-            "linkSize": "./assets/images/models/carpet/carpet_min.usdz#custom=https://checkitsmart.com/assets/banner/custom_banner_size_carpet.html&customHeight=medium",
+            "linkSize": "./assets/images/models/carpet/carpet_min.usdz#custom=https://checkitsmart.com/assets/banner/size/custom_banner_size_carpet.html&customHeight=medium",
             "figure": "./assets/images/models/carpet/carpet_size.svg",
             "linkBuy": "https://kvartal-e.ru/kover/vitebsk/hitset/manhatten/tproduct/158136639-444528739794-3727a6",
             "sizes": {w: 200, h: 300, d: 0.07}
